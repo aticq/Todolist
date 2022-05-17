b373d1bc515cec04092e545612554ad77fd8ab17
aticq <61829832+aticq@users.noreply.github.com>
2022-05-17 15:10:03 +0300

    Add files via upload

---
+++ precache-manifest.e2d0f551a4848adf593a4a8e6cb8199e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb716e1757178a3c5ebb78b87b4e00dd",
+    "url": "/Todolist/index.html"
+  },
+  {
+    "revision": "892d9e3fba08a180c15a",
+    "url": "/Todolist/static/css/main.d780d2b8.chunk.css"
+  },
+  {
+    "revision": "b38de254fbe23fc1d606",
+    "url": "/Todolist/static/js/2.9f42bcb6.chunk.js"
+  },
+  {
+    "revision": "1e1a1b03be40b91dea8526571963b8c7",
+    "url": "/Todolist/static/js/2.9f42bcb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "892d9e3fba08a180c15a",
+    "url": "/Todolist/static/js/main.0ecbcaab.chunk.js"
+  },
+  {
+    "revision": "23a8ead80f8150e06955",
+    "url": "/Todolist/static/js/runtime-main.ba498293.js"
+  }
+]);
